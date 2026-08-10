efbbb002834c40f6ea7a6ba7cdcb5feeac6a8f33
Balwant Singh Samant <bobysamant2007@gmail.com>
2026-08-10 21:51:23 +0530

    Add files via upload
    Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.

---
+++ code2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=20,b=10;
+    int sum=a+b;
+    printf("%d\n",sum);
+    int difference=a-b;
+    printf("%d\n",difference);
+    int product=a*b;
+    printf("%d\n",product);
+    int quotient=a/b;
+    printf("%d",quotient);
+
+    }
