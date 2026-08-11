65104c319e70238c7b0a4354e1b5078dbd9a8eb6
Balwant Singh Samant <bobysamant2007@gmail.com>
2026-08-11 19:31:03 +0530

    Add files via upload

---
+++ code3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int area,perimeter,length=10,breadth=5;
+    area=length*breadth;
+    perimeter=2*(length+breadth);
+   printf("areaa=%d\n",area);
+   ;
+   printf("perimeter=%d",perimeter);
+   
+}
